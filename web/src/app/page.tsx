02d38146f7f98e745d9d4d0a4ab6f6293ba0997d
koduka <koduka0715@gmail.com>
2024-08-17 20:10:26 +0900

    3dのサンプルを追加

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import { Box } from "@/libs/components/Box";
+import { Canvas3D } from "@/libs/components/Canvas3D";
 
 export default function Home() {
-  return <main>home</main>
+  return (
+    <Canvas3D>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box />
+    </Canvas3D>
+  );
 }
---
+++ web/src/libs/components/Canvas3D.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import type { CanvasProps } from "@react-three/fiber";
+
+import { Canvas } from "@react-three/fiber";
+
+export function Canvas3D(props: CanvasProps) {
+  return <Canvas {...props} />;
+}
