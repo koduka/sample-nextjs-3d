c5da8a230578f6a67b73091f339f32e117c3f234
koduka <koduka0715@gmail.com>
2024-08-20 21:21:55 +0900

    3dのサンプルを追加

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Box } from "@/libs/components/Box";
-import { Canvas3D } from "@/libs/components/Canvas3D";
+import { Canvas3D } from "@/libs/components/3d/Canvas3D";
+import { Box } from "@/libs/components/3d/models/Box";
 
 export default function Home() {
   return (
-    <Canvas3D>
+    <Canvas3D camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 1] }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight
         position={[10, 10, 10]}
